2a55a4ac014832e3ecd3e0b79d6effbadd78c6ba
Vlad Kleshko <vkoesko@gmail.com>
2024-02-18 18:23:07 +0200

    Made project security with using environment variables

--- newspapers_agency_service/settings.py
+++ newspapers_agency_service/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-cej#v5l4ql(8&e6ua1dtsh2mbz*!m9s#(#f4ytgxg_yusu3le4"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
