bcff6d31218d33b4072f1d629e62ddf6e1280cf1
Vlad Kleshko <vkoesko@gmail.com>
2024-05-16 17:05:34 +0300

    Fixed settings.py

--- newspapers_agency_service/settings.py
+++ newspapers_agency_service/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "publisher-insight.onrender.com"]
 
 # Application definition
 
